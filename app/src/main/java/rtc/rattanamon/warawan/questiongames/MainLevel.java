8f0bf3c590ba16df90c864a949f36c4ec1c9fa20
evewarawan <evewarawan@gmail.com>
2016-11-28 14:21:18 +0700

    you i ok

---
+++ app/src/main/java/rtc/rattanamon/warawan/questiongames/MainLevel.java
@@ -0,0 +1,13 @@
+package rtc.rattanamon.warawan.questiongames;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainLevel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_level);
+    }
+}
